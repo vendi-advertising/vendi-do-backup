ce7edc3aa8817407eabe6b61810671e5d7116e69
cjhaas <chris@vendiadvertising.com>
2018-01-19 14:57:00 -0600

    Version bump

--- app.php
+++ app.php
@@ -7,6 +7,6 @@ require VENDI_DO_BACKUP_PATH . '/includes/autoload.php';
 
 $backup_command = new Vendi\DigitalOcean\Backup\Commands\BackupCommand();
 
-$application = new Symfony\Component\Console\Application( 'Vendi Digital Ocean Backup', '1.0.2' );
+$application = new Symfony\Component\Console\Application( 'Vendi Digital Ocean Backup', '1.0.3' );
 $application->add( $backup_command );
 $application->run();
