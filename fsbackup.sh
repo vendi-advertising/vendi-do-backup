43c416361a41c50cce41515adabd97e76b92ae2b
Chris Haas <chris@vendiadvertising.com>
2018-09-27 09:04:06 -0500

    Update fsbackup.sh

--- fsbackup.sh
+++ fsbackup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-zip_file=/data/backups/www/`date +"%Y-%m-%d_%H-%M-%S"`.zip
-zip -r1 --quiet $zip_file /var/www -x "*.mp4" -x "*.webm" -x "*.ogv" -x "*.log" -x "*/.git/*" -x "*.mov" -x "*.wmv" -x "*/logs/*" -x "*/wfcache/*" -x "*/vendi_cache/*" -x "*/vendor/*" -x "*/node_modules/*"
+archive_file=/data/backups/www/`date +"%Y-%m-%d_%H-%M-%S"`.tar.gx
+tar zcvf $archive_file                      \
+    --exclude="*/logs/*"                    \
+    --exclude="*/node_modules/*"            \
+    --exclude="*/vendor/*"                  \
+    --exclude="*/wp-security-audit-log/*"   \
+    --exclude="*/wfcache/*"                 \
+    --exclude="*/vendi_cache/*"             \
+    --exclude="*/.git/*"                    \
+     /var/www/
 
 # Create 64 bytes of random data, asymetrically encrypt that with ssh public key write that two *.enc.key
 # Then use that key to symetrically encrypt the file to *.enc
-openssl rand 64 | tee >(openssl enc -aes-256-cbc -pass stdin -in $zip_file -out $zip_file.enc) | openssl rsautl -encrypt -pubin -inkey /vbin/ENCRYPTION_KEY_HERE -out $zip_file.enc.key
+openssl rand 64 | tee >(openssl enc -aes-256-cbc -pass stdin -in $archive_file -out $archive_file.enc) | openssl rsautl -encrypt -pubin -inkey /vbin/ENCRYPTION_KEY_HERE -out $archive_file.enc.key
 
-sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$zip_file.enc     --name=BACKUP_NAME_HERE_www_back
-sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$zip_file.enc.key --name=BACKUP_NAME_HERE_www_back_key
+sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$archive_file.enc     --name=BACKUP_NAME_HERE_www_back
+sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$archive_file.enc.key --name=BACKUP_NAME_HERE_www_back_key
 
 
 #Delete our temporary file
-rm $zip_file
+rm $archive_file
 
 #Prune files older than 5 days
 find /data/backups/www/ -mtime +5 -print0 | xargs -0 -r rm
