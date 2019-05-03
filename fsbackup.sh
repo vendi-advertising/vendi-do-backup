4153324575147e6d1c14b2df7f078157cf1a359f
Chris Haas <chris@vendiadvertising.com>
2019-05-03 16:41:32 -0500

    Update fsbackup.sh

--- fsbackup.sh
+++ fsbackup.sh
@@ -15,7 +15,7 @@ tar zcf $archive_file                      \
 
 # Create 64 bytes of random data, asymetrically encrypt that with ssh public key write that two *.enc.key
 # Then use that key to symetrically encrypt the file to *.enc
-openssl rand 64 | tee >(openssl enc -aes-256-cbc -pass stdin -in $archive_file -out $archive_file.enc) | openssl rsautl -encrypt -pubin -inkey /vbin/ENCRYPTION_KEY_HERE -out $archive_file.enc.key
+openssl rand 64 | tee >(openssl enc -aes-256-cbc -pass stdin -md sha512 -pbkdf2 -iter 1000 -in $archive_file -out $archive_file.enc) | openssl rsautl -encrypt -pubin -inkey /vbin/ENCRYPTION_KEY_HERE -out $archive_file.enc.key
 
 sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$archive_file.enc     --name=BACKUP_NAME_HERE_www_back
 sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$archive_file.enc.key --name=BACKUP_NAME_HERE_www_back_key
