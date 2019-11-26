8951ad49284261c45a0119af33ffe4804dee7914
Chris Haas <chris@vendiadvertising.com>
2019-11-26 08:16:24 -0600

    Ran against ShellCheck

--- fsbackup.sh
+++ fsbackup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-archive_file=/data/backups/www/`date +"%Y-%m-%d_%H-%M-%S"`.tar.gz
+archive_file=/data/backups/www/$(date +"%Y-%m-%d_%H-%M-%S").tar.gz
 cd /
-tar zcf $archive_file                      \
+tar zcf "$archive_file"                     \
     --exclude="*/logs/*"                    \
     --exclude="*/node_modules/*"            \
     --exclude="*/vendor/*"                  \
@@ -15,14 +15,14 @@ tar zcf $archive_file                      \
 
 # Create 64 bytes of random data, asymetrically encrypt that with ssh public key write that two *.enc.key
 # Then use that key to symetrically encrypt the file to *.enc
-openssl rand 64 | tee >(openssl enc -aes-256-cbc -pass stdin -md sha512 -pbkdf2 -iter 1000 -in $archive_file -out $archive_file.enc) | openssl rsautl -encrypt -pubin -inkey /vbin/ENCRYPTION_KEY_HERE -out $archive_file.enc.key
+openssl rand 64 | tee >(openssl enc -aes-256-cbc -pass stdin -md sha512 -pbkdf2 -iter 1000 -in "$archive_file" -out "$archive_file.enc") | openssl rsautl -encrypt -pubin -inkey /vbin/ENCRYPTION_KEY_HERE -out "$archive_file.enc.key"
 
-sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$archive_file.enc     --name=BACKUP_NAME_HERE_www_back
-sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file=$archive_file.enc.key --name=BACKUP_NAME_HERE_www_back_key
+sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file="$archive_file.enc"     --name=BACKUP_NAME_HERE_www_back
+sudo vendi-do-backup backup --private-key=PRIVATE_KEY_HERE --public-key=PUBLIC_KEY_HERE --bucket=vendi-backup --file="$archive_file.enc.key" --name=BACKUP_NAME_HERE_www_back_key
 
 
 #Delete our temporary file
-rm $archive_file
+rm "$archive_file"
 
 #Prune files older than 5 days
 find /data/backups/www/ -mtime +5 -print0 | xargs -0 -r rm
